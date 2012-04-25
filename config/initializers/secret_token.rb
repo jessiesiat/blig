211f8b13c5ffc527553c27434dbf8aafb87e4ebe
Jessie Siat <som_grad@yahoo.com>
2012-04-25 17:52:46 +0800

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blig::Application.config.secret_token = '1fa6edd9c905c08952a7774480623b2f5594165fa3c30303022f05a45eee0494a6e732dfbac83b6dab6b1217e9145700e6c38f0679320cbb8257498b2599039c'
